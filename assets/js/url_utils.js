b0dd2f70cc22d1e567ec0cc6df0d37c338a423ca
liangjin.bai <liangjin.bai@megatronix.co>
2023-07-12 09:49:48 +0800

    update bookmark r refactor js

---
+++ assets/js/url_utils.js
@@ -0,0 +1,12 @@
+const host_dict = {
+    "1271": "114.116.12.71",
+    "1431": "114.116.14.31",
+    "34252": "123.249.34.252",
+    "asus": "bytedance.asuscomm.com",
+};
+
+function open_url(host, port, type = "http") {
+    const real_host = host_dict[host] || host;
+    const url = `${type}://${real_host}:${port}`;
+    window.open(url, "_blank");
+}
